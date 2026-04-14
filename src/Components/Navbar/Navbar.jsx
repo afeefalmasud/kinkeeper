8398b5a04de7636d89faed860f9db8856ad2569b
afeefalmasud <its.afeefalmasud@gmail.com>
2026-04-15 02:28:03 +0600

    added navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { NavLink } from 'react-router'
+import "./Navbar.css"
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='shadow-md'>
+      <div className='container mx-auto'>
+        <div className="navbar">
+          <div className="flex-1">
+            <img src="/assets/logo.png" alt="" />
+          </div>
+          <div className="flex-none">
+            <ul className="menu menu-horizontal gap-4 px-1 font-semibold text-[#64748B]">
+              <li>
+                <NavLink to="/"> <i className="fa-regular fa-house"></i> Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/timeline"> <i className="fa-regular fa-clock"></i> Timeline</NavLink>
+              </li>
+              <li>
+                <NavLink to="/stats"> <i className="fa-solid fa-chart-line"></i> Stats</NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,19 @@ import './index.css'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import { Root } from './Components/Root/Root';
+import { Home } from './Components/Home/Home';
+import { Timeline } from './Components/Timeline/Timeline';
+import { Stats } from './Components/Stats/Stats';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    Component: Root
+    Component: Root,
+    children: [
+      {index: true, Component: Home},
+      {path: "timeline", Component: Timeline},
+      {path:"stats", Component: Stats},
+    ]
   }
 ])
 
